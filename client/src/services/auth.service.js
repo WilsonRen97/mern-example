30926bc2a0d20e2a803fa1c0b198d0aa2066322a
Wilson <s9208892088@gmail.com>
2022-10-23 09:51:26 -1000

    change service url2

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,13 +1,5 @@
 import axios from "axios";
-const configs = {
-  development: {
-    SERVER_URI: "http://localhost:8080/",
-  },
-  production: {
-    SERVER_URI: "https://project9mern.herokuapp.com/",
-  },
-};
-const API_URL = configs[process.env.NODE_ENV] + "/api/user";
+const API_URL = "https://project9mern.herokuapp.com/api/user";
 
 class AuthService {
   login(email, password) {
--- client/src/services/course.service.js
+++ client/src/services/course.service.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const configs = {
-  development: {
-    SERVER_URI: "http://localhost:8080/",
-  },
-  production: {
-    SERVER_URI: "https://project9mern.herokuapp.com/",
-  },
-};
-const API_URL = configs[process.env.NODE_ENV] + "/api/courses";
+// const configs = {
+//   development: {
+//     SERVER_URI: "http://localhost:8080/",
+//   },
+//   production: {
+//     SERVER_URI: "https://project9mern.herokuapp.com/",
+//   },
+// };
+const API_URL = "https://project9mern.herokuapp.com/api/courses";
 
 class CourseService {
   post(title, description, price) {
